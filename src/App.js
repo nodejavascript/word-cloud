7aa9e47eb79733978996972f7646b62e323ff6d5
nodejavascript <engineering@nodejavascript.com>
2023-04-15 14:22:28 -0400

    rn layout dir

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ReactGA from 'react-ga4'
 
 import './App.css'
 
-import SiteWrapper from './components/SiteWrapper'
+import SiteWrapper from './layout/SiteWrapper'
 import Site from './site/Site'
 
 const { REACT_APP_MEASUREMENT_ID } = process.env
